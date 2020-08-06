f91f43197f076f28e28ba4397740893dcb51bc56
Kaima <244241917@qq.com>
2020-08-06 09:05:41 +0800

    Time To Collision

---
+++ Project/Assets/VelocityPlanning/Scripts/Agent.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    [SerializeField] private float m_radius;
+    [SerializeField] private Transform m_target;
+    [SerializeField] private float m_speed;
+    [SerializeField] private float m_maxForce = 10f;
+
+    /// <summary>
+    /// Agent能检测到多大范围内的邻居
+    /// </summary>
+    [SerializeField] private float m_sensingRadius = 5;
+
+    /// <summary>
+    /// 只考虑该时间范围内会发生的碰撞
+    /// 进攻性强的可以设小一点，内向的可以设大一点
+    /// </summary>
+    [SerializeField] private float m_timeHorizon = 4f;
+
+    public float Radius { get { return m_radius; } }
+    public Vector2 Velocity { get; set; }
+    public Vector2 Force { get; set; }
+    public float TimeHorizon { get { return m_timeHorizon; } }
+    public float SensingRadius { get { return m_sensingRadius; } }
+    public float MaxForce { get { return m_maxForce; } }
+
+    public Vector2 GoalVelocity
+    {
+        get
+        {
+            if (m_target != null)
+            {
+                Vector2 p = new Vector2(m_target.position.x, m_target.position.z);
+                Vector2 dir = (p - Pos2).normalized;
+                return dir * m_speed;
+            }
+            else
+            {
+                return Vector2.zero;
+            }
+        }
+    }
+
+    public Vector2 Pos2
+    {
+        get
+        {
+            var pos3 = transform.position;
+            return new Vector2(pos3.x, pos3.z);
+        }
+
+        set
+        {
+            transform.position = new Vector3(value.x, 0, value.y);
+        }
+    }
+
+    private void Start()
+    {
+        TimeToCollisionManager.Instance.Register(this);
+    }
+
+    private void OnDestroy()
+    {
+        TimeToCollisionManager.Instance.Unregister(this);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (m_target != null)
+            Gizmos.DrawLine(transform.position, m_target.position);
+    }
+}
---
+++ Project/Assets/VelocityPlanning/Scripts/TimeToCollisionManager.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// 预测出什么时候会发生碰撞，然后进行规避
+/// </summary>
+public class TimeToCollisionManager : MonoBehaviour
+{
+    public static TimeToCollisionManager Instance;
+
+    private readonly List<Agent> m_agents = new List<Agent>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        UpdateAllAgents();
+    }
+
+    public void Register(Agent agent)
+    {
+        m_agents.Add(agent);
+    }
+
+    public void Unregister(Agent agent)
+    {
+        m_agents.Remove(agent);
+    }
+
+    /// <summary>
+    /// 什么时候会发生碰撞
+    /// 参考：Game AI Pro 2 Chapter19
+    /// </summary>
+    private static float TimeToCollision(Agent i, Agent j)
+    {
+        //满足 ||(Pb + Vb * t) - (Pa + Va * t)|| = Ra + Rb 时，a和b发生碰撞
+        //进行一些分解后就能得到一个二元二次方程，然后使用求解公式即可
+
+        float r = i.Radius + j.Radius;
+        Vector2 w = i.transform.position - j.transform.position;
+        float c = Vector2.Dot(w, w) - r * r;
+        if (c < 0) //agents are colliding
+            return 0;
+
+        Vector2 v = i.Velocity - j.Velocity;
+        float a = Vector2.Dot(v, v);
+        float b = Vector2.Dot(w, v);
+        float discr = b * b - a * c;
+        if (discr <= 0)
+            return float.MaxValue;
+
+        float tau = (b - Mathf.Sqrt(discr)) / a;
+        if (tau < 0)
+            return float.MaxValue;
+
+        return tau;
+    }
+
+    private void UpdateAllAgents()
+    {
+        foreach(Agent agent in m_agents)
+        {
+            agent.Force = 2 * (agent.GoalVelocity - agent.Velocity);
+
+            List<Agent> neighbors = FindNeighbors(agent);
+            foreach(var neighbor in neighbors)
+            {
+                float t = TimeToCollision(agent, neighbor);
+
+                //计算力方向（就是邻居指向自己的向量）
+                Vector2 avoidForce = agent.Pos2 + agent.Velocity * t - neighbor.Pos2 - neighbor.Velocity * t;
+                if (avoidForce != Vector2.zero)
+                    avoidForce.Normalize();
+
+                //计算力大小（越接近0越大，超过TimeHorizon则为0）
+                float mag = 0;
+                if (t >= 0 && t <= agent.TimeHorizon)
+                    mag = (agent.TimeHorizon - t) / (t + 0.001f);
+                if (mag > agent.MaxForce)
+                    mag = agent.MaxForce;
+                avoidForce *= mag;
+
+                agent.Force += avoidForce;
+            }
+        }
+
+        foreach(Agent agent in m_agents)
+        {
+            agent.Velocity += agent.Force * Time.deltaTime;
+            agent.Pos2 += agent.Velocity * Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// 寻找某个单位附近的邻居
+    /// 可以考虑使用四叉树等优化性能
+    /// </summary>
+    private List<Agent> FindNeighbors(Agent curt)
+    {
+        List<Agent> result = new List<Agent>();
+
+        float sqrSensingDist = curt.SensingRadius * curt.SensingRadius;
+        for(int i = 0; i < m_agents.Count; i++)
+        {
+            Agent neighbor = m_agents[i];
+            if (neighbor == curt)
+                continue;
+
+            float sqrDistance = (neighbor.Pos2 - curt.Pos2).sqrMagnitude;
+            if (sqrDistance <= sqrSensingDist)
+                result.Add(neighbor);
+        }
+
+        return result;
+    }
+}
